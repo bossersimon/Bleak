f91761ea9c221d750f95b22f69ada96f04f455a6
bossersimon <simon.bosser@gmail.com>
2025-03-10 18:32:44 +0100

    concurrency seems to work, but not plotting

--- pyqt_read.py
+++ pyqt_read.py
@@ -10,9 +10,40 @@ from bleak import BleakClient
 import numpy as np
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtWidgets
+import sys
 
 address = "28:37:2F:6A:B1:42"
 CHARACTERISTIC_UUID = "c1756f0e-07c7-49aa-bd64-8494be4f1a1c"
+
+
+class BLEWorker(QtCore.QObject):
+    data_received = QtCore.pyqtSignal(float)
+
+    def __init__(self, address):
+        super().__init__()
+        self.address = address
+        self.loop = None
+
+# called when new data is received
+    async def notification_handler(self, sender, data):
+        x, y, z = struct.unpack('<fff', data)
+        print(f"received data: {x, y, z}")
+        self.data_received.emit(x)
+    
+    async def read_BLE(self):
+        # Connect to ESP
+        async with BleakClient(address) as client:
+            await client.start_notify(CHARACTERISTIC_UUID, self.notification_handler)
+
+            # keep script running
+            while True:
+                await asyncio.sleep(0.1)  # Prevents blocking, allows other tasks to run
+
+    def run(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_until_complete(self.read_BLE())
+
 
 class RealTimePlot:
 
@@ -34,48 +65,32 @@ class RealTimePlot:
         self.timer.timeout.connect(self.update) 
         self.timer.start(50) # 50ms update
 
+        self.thread = QtCore.QThread()
+        self.ble_worker = BLEWorker(address)
+        self.ble_worker.moveToThread(self.thread)
+
+        self.ble_worker.data_received.connect(self.read_data)
+
+        self.thread.started.connect(self.ble_worker.run)
+        self.thread.start()
+        
         self.latest_data = 0.0
 
-        self.loop = asyncio.new_event_loop()
-        #self.thread = QtCore.QThread()
-        asyncio.set_event_loop(self.loop)
-        self.loop.create_task(self.read_BLE())
-        self.loop.run_forever()
-
-
-        #self.ble_task = asyncio.create_task(self.read_BLE())
+    def read_data(self, new_data):
+        self.latest_data = new_data
 
     def update(self):
-        print("update!")
-
         self.data = np.roll(self.data, -1)
         self.data[-1] = self.latest_data
         self.curve.setData(self.data) # update
 
 
-# called when new data is received
-    async def notification_handler(self, sender, data):
-        x, y, z = struct.unpack('<fff', data)
-
-#        self.latest_data = (x,y,z)
-        self.latest_data = x
-        print(f"received data: {x, y, z}")
-    
-
-    async def read_BLE(self):
-        # Connect to ESP
-        async with BleakClient(address) as client:
-            await client.start_notify(CHARACTERISTIC_UUID, self.notification_handler)
-
-            # keep script running
-            while True:
-                await asyncio.sleep(0.1)  # Prevents blocking, allows other tasks to run
 
 #async def main():
 
 if __name__ == "__main__":
     plot = RealTimePlot()
-    plot.app.exec()
+    sys.exit(plot.app.exec())
 
 #asyncio.run(main())
 
