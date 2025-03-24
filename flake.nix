0ce29106ff7c08b9437dd0369b62a996a11a0958
bossersimon <simon.bosser@gmail.com>
2025-03-24 10:15:33 +0100

    moved into direnv

---
+++ flake.nix
@@ -0,0 +1,44 @@
+
+{
+  description = "Qt6 + Python Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flakeutils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flakeutils }: 
+    flakeutils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+
+      python = pkgs.python312;
+
+      in
+
+      # Some python packages have dependencies that 
+      # are broken on 32-bit systems. Hence, 
+      # we have this if case here. We have no results
+      # in this flake for such systems. 
+      if !(pkgs.lib.hasInfix "i686" system) then {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            (python.withPackages (p: [
+              p.pyqtgraph
+              p.pyqt6
+              p.qasync
+
+              p.bleak
+            ]))
+            pkgs.qt6.qtbase
+#            pkgs.qt6.wrapQtAppsHook
+            pkgs.gtk-engine-murrine
+          ];
+          shellHook = ''
+            export QT_PLUGIN_PATH=${pkgs.qt6.qtbase}/lib/qt-*/plugins
+            export QT_QPA_PLATFORM_PLUGIN_PATH=${pkgs.qt6.qtbase}/lib/qt-*/plugins
+          '';
+        };
+
+      } else {}
+    );
+}
