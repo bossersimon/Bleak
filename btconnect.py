55f437ee066380f665c3f751bda43e95f8c4ce6a
bossersimon <simon.bosser@gmail.com>
2025-04-09 08:47:06 +0200

    much stuff

--- btconnect.py
+++ btconnect.py
@@ -9,7 +9,10 @@ import matplotlib.pyplot as plt
 #from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
 import csv
+from multiprocessing import Process, Queue
 
+#import time
+import numpy as np
 
 address = "28:37:2F:6A:B1:42"
 CHARACTERISTIC_UUID = "c1756f0e-07c7-49aa-bd64-8494be4f1a1c"
@@ -21,54 +24,50 @@ scatter = ax.scatter([],[],[])
     # Initialize coordinates
 gx, gy, gz = [],[],[]
 latest_coordinates = None
+dt = 0.01
+velocity = np.array([0.0, 0.0, 0.0])
+pos = np.array([0.0, 0.0, 0.0])
+
+
+# called when new data is received
+async def notification_handler(sender, data):
+    x, y, z = struct.unpack('<fff', data)
+
+    velocity += [x, y, z] * dt
+    pos += velocity * dt
+
+    latest_coordinates = (x,y,z)
+    print(f"received data: {x, y, z}")
+    
 
 async def read_BLE(address):
     #dq = deque([]) 
-
     global latest_coordinates
     # Connect to ESP
     async with BleakClient(address) as client:
-        #with open('data.csv', 'a') as file:
-         #   csv_writer = csv.writer(file)
+        await client.start_notify(CHARACTERISTIC_UUID, notification_handler)
 
-            # read data from ESP, write to csv
+        # keep script running
         while True:
-            data = await client.read_gatt_char(CHARACTERISTIC_UUID)
-            if data:
-                #value = int.from_bytes(data, byteorder='little')
-                x, y, z = struct.unpack('<hhh', data)
-                latest_coordinates = (x,y,z)
-                print(f"received data: {x, y, z}")
             await asyncio.sleep(0.1)  # Prevents blocking, allows other tasks to run
-
-          #          csv_writer.writerow(gx,gy,gz)
                     #dq.extend([gx, gy, gz]);
                     #dq.popleft()
 
 def init():
-    ax.set(xlim=[0, 250], ylim=[0, 250], zlim = [0, 250],\
+    ax.set(xlim=[-10, 10], ylim=[-10, 10], zlim = [-10, 10],\
             xlabel='gx', ylabel='gy', zlabel = 'gz')
     return scatter,
 
 def update(frame):
-    # update data
-    #data = pd.read_csv('data.csv')
-    #data = data.tail(1)
-#    x = data[0]
-#    y = data[1]
-#    z = data[2]
+
     if latest_coordinates != None:
         x, y, z = latest_coordinates
         gx.append(x)
         gy.append(y)
         gz.append(z)
 
-    #ax.scatter(gx, gy, gz)
     scatter._offsets3d = (gx, gy, gz)
-    #scatter(gx ,gy, gz)
-    #ax.legend()
-    # update plot
-    #data = np.stack([x,y,z]).T
+
     return scatter,
 
 
@@ -85,7 +84,6 @@ async def main(address):
         await asyncio.sleep(0.01)  # Allow async event loop to process other tasks (non-blocking)
 
     await ble_task
-
 
 asyncio.run(main(address))
 
