1a474e0b5dd4367c54aa0081a18c092127f297d6
bossersimon <simon.bosser@gmail.com>
2025-04-09 11:45:09 +0200

    added phase in generated signal

--- g_estimator.py
+++ g_estimator.py
@@ -6,11 +6,26 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore
 from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout
 from scipy.fft import fft, ifft, fftshift, fftfreq
-
 from scipy.signal import butter,lfilter,filtfilt
-
-import csv
 import math
+
+from bleak import BleakClient
+import asyncio
+#import sys
+import qasync
+import signal
+
+"""
+address = "DC:1E:D5:1B:E9:FE" # ESP MAC address
+CHARACTERISTIC_UUID = "c1756f0e-07c7-49aa-bd64-8494be4f1a1c" # Data characteristic
+PARAMS_UUID = "97b28d55-f227-4568-885a-4db649a8e9fd" # Parameter characteristic
+
+# Scale parameters
+acc_divider = 16384
+gyro_divider = 131
+dividers = [acc_divider, acc_divider, acc_divider, gyro_divider, gyro_divider, gyro_divider]
+bias_values = [0,0,0]
+"""
 
 class PlotWindow(QWidget):
 
@@ -63,11 +78,23 @@ class PlotWindow(QWidget):
         self.latest_data = np.empty((6,0)) 
         self.data6= np.zeros(self.bufferSize)
 
+        """
+        self.loop = loop
+        self.ble_worker = BLEWorker(address,loop)
+        self.ble_worker.data_received.connect(self.read_data)
+        self.ble_worker.start_ble()
+        """
+
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.update)
         self.timer.start(50)
 
         self.counter = 0
+
+    """
+    def read_data(self, new_data):
+        self.latest_data = np.array(new_data).reshape(6,-1)
+    """
         
     def update(self):
         i = self.counter
@@ -79,23 +106,22 @@ class PlotWindow(QWidget):
 
         chunk_size = 100
         fs = 100
+        t1 = np.arange(chunk_size,dtype=float)/fs
+
+        freqs = fftshift(fftfreq(100, d = 1/fs))
 
         if i<=990:
-            t1 = np.arange(chunk_size,dtype=float)/fs
-            freqs = fftshift(fftfreq(100, d = 1/fs))
-
             self.data1 = self.latest_data[0] # acc_x
             self.data2 = self.latest_data[1] # acc_y
 
-            c1 = self.data1[i*10:i*10+100]
-            c2 = self.data2[i*10:i*10+100]
+            d1 = self.data1[i*10:i*10+100]
+            d2 = self.data2[i*10:i*10+100]
 
             d1 = output_signal = filtfilt(b, a, c1)
             d2 = output_signal = filtfilt(b, a, c2)
 
             f1 = fftshift(fft(c1)/len(c1)) # fft_x
             f2 = fftshift(fft(c2)/len(c2)) # fft_y
-
 
             f1[np.abs(f1)<1e-6]=0
             f2[np.abs(f2)<1e-6]=0
@@ -122,11 +148,7 @@ class PlotWindow(QWidget):
             self.data6 = self.latest_data[5]
             """
 
-            """
-            # shift old data
-            self.data1 = np.roll(self.data1, -chunk_size)
-            self.data2 = np.roll(self.data2, -chunk_size)
-            self.data3 = np.roll(self.data3, -chunk_size)
+            """ # shift old data self.data1 = np.roll(self.data1, -chunk_size) self.data2 = np.roll(self.data2, -chunk_size) self.data3 = np.roll(self.data3, -chunk_size)
             self.data4 = np.roll(self.data4, -chunk_size)
 
             # read new data into data buffers
@@ -151,6 +173,55 @@ class PlotWindow(QWidget):
 
 #        self.latest_data = np.empty((4, 0))
 
+"""
+# Manages BLE communication and reads data asynchronously
+class BLEWorker(QtCore.QObject):
+    data_received = QtCore.pyqtSignal(list)
+
+    def __init__(self, address, loop, parent = None):
+        super().__init__()
+        self.loop = loop
+        self.address = address #ESP MAC
+
+    # Called when new data is received
+    async def notification_handler(self, sender, data):
+        
+        received = convert_to_float(data)
+        #print(f"shape received: {np.shape(received)}\n")
+        
+        self.data_received.emit(received.flatten().tolist()) # emits to read_data()
+
+
+    async def read_ble(self):
+        # Connect to ESP
+        async with BleakClient(self.address) as client:
+
+            #a_scale = ACC_SCALES["2G"]
+            #g_scale = GYRO_SCALES["250DPS"]
+
+            # send scale parameters to ESP
+            #scales = bytes([a_scale, g_scale], 'big')
+            #await client.write_gatt_char(PARAMS_UUID, scales)
+
+            # for bias and scale correction
+            param_data = await client.read_gatt_char(PARAMS_UUID)
+            global bias_values
+            bias_values = [int.from_bytes(param_data[i:i+2], 'little', signed=True) / 100 for i in range(0, len(param_data), 2)]
+
+            # print("Adjustment values:", bias_values)
+
+            await client.start_notify(CHARACTERISTIC_UUID, self.notification_handler)
+            
+            # print("start notify complete")
+
+            while True:
+                await asyncio.sleep(0.1)
+
+    # create tasks
+    def start_ble(self):
+        asyncio.run_coroutine_threadsafe(self.read_ble(), self.loop)  # Submit coroutine to loop 
+
+
 
 def convert_to_float():
    
@@ -168,13 +239,14 @@ def convert_to_float():
 #    argx = 
 
     return scaled
-
+"""
 
 def generate_signals(plot):
     fs = 100 # sampling frequency
     T = 100 # signal length
     N = T*fs # number of samples
     f = 10
+    phi = np.pi/6
 
     order = 6
         # . One gotcha is that Wn is a fraction of the Nyquist frequency. So if the sampling rate is 1000Hz and you want a cutoff of 250Hz, you should use Wn=0.5
@@ -187,10 +259,10 @@ def generate_signals(plot):
     window = 1
     t1 = np.arange(N,dtype=float)/fs
     c1 = np.random.normal(size=N)
-    c1 += 10*np.sin(2*np.pi *f*t1)*window
+    c1 += 10*np.sin(2*np.pi *f*t1+phi)*window
 
     c2 = np.random.normal(size=N)
-    c2 += 10*np.cos(2*np.pi*f*t1)*window
+    c2 += 10*np.cos(2*np.pi*f*t1+phi)*window
 
     d1 = output_signal = filtfilt(b, a, c1)
     d2 = output_signal = filtfilt(b, a, c2)
@@ -216,7 +288,7 @@ def generate_signals(plot):
     plot.latest_data = rows
 
 def read_recording(plot):
-    loaded_data = np.loadtxt("recording.txt", delimiter = ",")
+    loaded_data = np.loadtxt("recording1.txt", delimiter = ",")
 #    print(f"loaded_data shape: {np.shape(loaded_data)}\n")
     
     loaded_data = loaded_data.T
@@ -226,19 +298,13 @@ def read_recording(plot):
  #   plot.latest_data[:2,:] = loaded_data
     plot.latest_data = loaded_data
 
-
 if __name__ == "__main__":
     app = pg.mkQApp()
     plot = PlotWindow()
 
-    generate_signals(plot)
-    #read_recording(plot)
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    #generate_signals(plot)
+    read_recording(plot)
 
     plot.show()
     app.exec()
-
-"""
-        lines = file.readlines()
-        for line in lines:
-            datapoint = (line.strip()).split(",")
-"""
